120f5daaac3d0f3b6ee88f1b289b71a52ce338db
Brandon Sandrowicz <brandon@sandrowicz.org>
2012-06-28 09:43:42 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+
+try:
+    from setuptools import setup
+except ImportError:
+    print "Falling back to distutils. Functionality may be limited."
+    from distutils.core import setup
+
+config = {
+    'description'  : 'Suite of command-line tools for dealing with BitTorrent files',
+    'url'          : 'http://github.com/bsandrow/bttools',
+    'author'       : 'Brandon Sandrowicz',
+    'author_email' : 'brandon@sandrowicz.org',
+    'version'      : '0.1',
+    'packages'     : [],
+    'name'         : 'bttools',
+    'test_suite'   : '',
+}
+
+setup(**config)
